3f06cde310fff64c3a92072f188aee306c3add61
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-07-10 19:13:36 +0530

    Added service layer and dependency injection

--- src/main/java/com/dhiraj/student_management/controller/StudentController.java
+++ src/main/java/com/dhiraj/student_management/controller/StudentController.java
@@ -1,13 +1,18 @@
 package com.dhiraj.student_management.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.dhiraj.student_management.service.StudentService;
 
 @RestController
 public class StudentController {
 
+	@Autowired
+	private StudentService studentService;
+
 	@GetMapping("/")
 	public String home() {
-		return "Welcome to Student Management System";
+		return studentService.getMessage();
 	}
 
 }
---
+++ src/main/java/com/dhiraj/student_management/service/StudentService.java
@@ -0,0 +1,10 @@
+package com.dhiraj.student_management.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    public String getMessage() {
+        return "Student Service is working successfully!";
+    }
+
+}

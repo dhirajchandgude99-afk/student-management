4b165c94d41034124a16901f5b5d173fbc07290d
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-07-10 20:49:07 +0530

    Added POST API using @PostMapping and @RequestBody

--- src/main/java/com/dhiraj/student_management/controller/StudentController.java
+++ src/main/java/com/dhiraj/student_management/controller/StudentController.java
@@ -3,6 +3,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.dhiraj.student_management.service.StudentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.dhiraj.student_management.entity.Student;
 
 @RestController
 public class StudentController {
@@ -13,6 +16,11 @@ public class StudentController {
 	@GetMapping("/")
 	public String home() {
 		return studentService.getMessage();
+	}
+	@PostMapping("/student")
+	public String addStudent(@RequestBody Student student) {
+		// Logic to add the student to the database or perform any other operation
+		return studentService.addStudent();
 	}
 
 }
--- src/main/java/com/dhiraj/student_management/service/StudentService.java
+++ src/main/java/com/dhiraj/student_management/service/StudentService.java
@@ -3,6 +3,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+    public String addStudent() {
+        return "Student added successfully!";
+    }
     public String getMessage() {
         return "Student Service is working successfully!";
     }

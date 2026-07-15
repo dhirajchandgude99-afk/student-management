40626b14f325b226f39fea96ef09c6b8ac6337be
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-07-15 21:32:55 +0530

    Added Lombok annotations and reduced boilerplate code

---
+++ src/main/java/com/dhiraj/student_management/entity/Employee.java
@@ -0,0 +1,22 @@
+package com.dhiraj.student_management.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Employee {
+ private int id;
+ private String name;
+ private double salary;
+ public static void main(String[] args) {
+
+    Employee emp = new Employee(101, "Dhiraj", 50000);
+
+    System.out.println(emp);
+    
+}
+}
+
